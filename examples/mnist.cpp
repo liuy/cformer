0ba0a8563dad8f9b779336a487331504d02fe035
Liu Yuan <namei.unix@gmail.com>
2023-08-16 21:20:38 +0800

    cformer: add mnist.cpp example
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>

---
+++ examples/mnist.cpp
@@ -0,0 +1,33 @@
+#include "../cformer.h"
+
+int main(int argc, char* argv[])
+{
+    seqnet model {
+        new Linear(28*28, 300, ReLU),
+        new BatchNorm1d(300),
+        //new Dropout(0.1),
+        new Linear(300, 100, ReLU),
+        new Linear(100, 10, Softmax),
+    };
+    data set(mnist_reader);
+    set.load({
+        new random_rotate(30),
+        new elastic_transform(5,4,1),
+    });
+    af::timer t = af::timer::start();
+    SGD op(model.params, 2e-4);
+    trainer tr = {
+        .epochs = 18,
+        .batch_size = 512,
+        .optimizer = op,
+        .loss_fn = categorical_cross_entropy,
+        .metrics_fn = categorical_accuracy,
+    };
+    model.summary();
+    model.train(set, tr);
+
+    tensor y_pred = model(set.test_x);
+    float accu = categorical_accuracy(set.test_y, y_pred);
+    printf("\nTotal time %.1fs, MNIST Test accuracy: %.4f\n", af::timer::stop(t), accu);
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,10 +1,20 @@
 MAKEFLAGS += --no-print-directory
 
+# make run: run the main program
+# make mnist: run the mnist example
+# make test: run the test cases
+# make clean: clean the build directory
+
 debug := off # make run debug=on to enable debug mode
 
 all: cformer
 cformer:
 	@cmake -B build -DCF_DEBUG=$(debug) && cmake --build build -t cformer
+
+mnist:
+	@cmake -B build -DCF_MNIST=on && cmake --build build -t cformer_mnist
+	@echo "\n==========program output==========\n"
+	@build/cformer_mnist
 
 run: cformer
 	@echo "\n==========program output==========\n"
