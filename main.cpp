05c9f90e355d484af1cfff45edafd2ae7478ed46
Liu Yuan <namei.unix@gmail.com>
2023-08-30 20:49:33 +0800

    remove tensor2idx()
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@ static tensor idx2tensor(uint32_t i)
     return tensor(array(1, &i).as(f32));
 }
 
-static uint32_t tensor2idx(const tensor &t)
-{
-    return argmax(t.data).as(u32).scalar<uint32_t>();
-}
-
 static void generate(seqnet &model, tokenizer &tok, const std::string &prompt, int n)
 {
     tensor x;
@@ -22,7 +17,7 @@ static void generate(seqnet &model, tokenizer &tok, const std::string &prompt, i
 
     printf("\nText generation:\n");
     std::cout << prompt;
-    uint32_t idx = tensor2idx(x);
+    uint32_t idx = logits_sample_next(x.data, 1, 0.0);
     std::cout << tok.idx2token[idx];
     x = idx2tensor(idx);
     for (int i = 0; i < n; i++) {
